db0208a99ed14e4688cb20ae99ab56106659a688
Sabin Adams <sabintheworld@gmail.com>
2021-05-20 19:26:13 -0700

    Initial commit

---
+++ src/fontawesome.js
@@ -0,0 +1,10 @@
+// Import the library
+import { library } from '@fortawesome/fontawesome-svg-core'
+// Import whichever icons you want to use
+import { faRocket, faHome } from '@fortawesome/free-solid-svg-icons'
+
+// Add the icons to your library
+library.add(
+    faRocket,
+    faHome
+)
